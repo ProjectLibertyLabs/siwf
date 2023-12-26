4a662fb07af6ccc4db6335784df6ce39a3fb848b
Joe Caputo <joseph.caputo@unfinished.com>
2023-12-26 12:38:54 -0500

    fix: apply formatting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["packages/**"],
-  extends: ["@repo/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
+  ignorePatterns: ['packages/**'],
+  extends: ['@repo/eslint-config/library.js'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
