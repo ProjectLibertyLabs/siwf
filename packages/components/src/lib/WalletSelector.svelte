a98998b06cd34196793b4ebf507174ae6b9c07c2
Joe Caputo <joseph.caputo@unfinished.com>
2024-01-11 10:51:51 -0500

    fix: vite dev errors

--- packages/components/src/lib/WalletSelector.svelte
+++ packages/components/src/lib/WalletSelector.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
   import type { Extension } from './extensionsConfig.ts';
   import { onMount } from 'svelte';
-  import { extensionsConfig, Extension } from './extensionsConfig.js';
+  import { extensionsConfig } from './extensionsConfig.js';
   import WalletButton from './WalletButton.svelte';
 
-  export let extensions: Extension[] = [];
+  export let extensions: Extension[] = extensionsConfig;
   export let onSelectedWallet: (event: CustomEvent) => Promise<void>;
-
-  onMount(() => {
-    extensions = extensionsConfig;
-  });
 </script>
 
 <div class="xs:mx-12 sm:w-500 md:w-800 mx-auto flex flex-col gap-2">
