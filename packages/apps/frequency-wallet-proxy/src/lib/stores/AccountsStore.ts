7aa7545305af273216612befc93c9dac8cb7c9f9
Joe Caputo <joseph.caputo@unfinished.com>
2024-02-07 17:36:05 -0500

    fix: move AccountStore back to its own file

--- packages/apps/frequency-wallet-proxy/src/lib/stores/AccountsStore.ts
+++ packages/apps/frequency-wallet-proxy/src/lib/stores/AccountsStore.ts
@@ -1,45 +1,34 @@
-import { derived, readable } from 'svelte/store';
+import { derived } from 'svelte/store';
 import { type ConnectedExtensionMap, ConnectedExtensionsStore } from '.';
+import type { InjectedAccount } from '@polkadot/extension-inject/types';
 
 /// Store that maps public key address to the names of wallet extensions that provide it
 
-export class AccountMap extends Map<string, Set<string>> {
+export interface InjectedAccountWithExtensions extends InjectedAccount {
+  wallets: Set<string>;
+}
+
+export class AccountMap extends Map<string, InjectedAccountWithExtensions> {
   constructor() {
     super();
   }
 
-  public updateAccount(address: string, wallet: string) {
-    const set = this.get(address) ?? new Set<string>();
-    set.add(wallet);
-    this.set(address, set);
+  public updateAccount(account: InjectedAccount, wallet: string) {
+    const value = this.get(account.address) ?? { ...account, wallets: new Set<string>() };
+    value.wallets.add(wallet);
+    this.set(account.address, value);
   }
 }
 
-// export const AccountsStore = readable(new AccountMap(), (set) => {
-//   const unsub = ConnectedExtensionsStore.subscribe(async (ConnectedExtensionPromise) => {
-//     const accountMap: AccountMap = new AccountMap();
-//     const extensionMap: ConnectedExtensionMap = await ConnectedExtensionPromise;
-//     for (const extension of [...extensionMap.values()]) {
-//       for (const account of extension.accounts) {
-//         accountMap.updateAccount(account.address, extension.injectedName);
-//       }
-//     }
-
-//     set(accountMap);
-//   });
-//   return () => unsub();
-// });
-
-// export const AccountsStore = derived([ConnectedExtensionsStore], ([$ConnectedExtensionsStore]) =>
-//   (async () => {
-//     const accountMap: AccountMap = new AccountMap();
-//     const extensionMap: ConnectedExtensionMap = await $ConnectedExtensionsStore;
-//     for (const extension of [...extensionMap.values()]) {
-//       for (const account of extension.accounts) {
-//         accountMap.updateAccount(account.address, extension.injectedName);
-//       }
-//     }
-
-//     return accountMap;
-//   })()
-// );
+export const AccountsStore = derived([ConnectedExtensionsStore], ([$ConnectedExtensionsStore]) =>
+  (async () => {
+    const accountMap: AccountMap = new AccountMap();
+    const extensionMap: ConnectedExtensionMap = await $ConnectedExtensionsStore;
+    for (const extension of [...extensionMap.values()]) {
+      for (const account of extension.accounts) {
+        accountMap.updateAccount(account, extension.injectedName);
+      }
+    }
+    return accountMap;
+  })()
+);
--- packages/apps/frequency-wallet-proxy/src/lib/stores/index.ts
+++ packages/apps/frequency-wallet-proxy/src/lib/stores/index.ts
@@ -1,3 +1,4 @@
+export * from './AccountsStore';
 export * from './CachedExtensionsStore';
 export * from './ConfiguredExtensionsStore';
 export * from './ConnectedExtensionsStore';
