8e29b4526b740fe44f2e25b2c3a8b0699324c481
Joe Caputo <joseph.caputo@unfinished.com>
2024-03-19 15:44:36 -0400

    feat: add skeleton publishable package

---
+++ packages/siwf/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'pathe';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [dts({ insertTypesEntry: true })],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'siwf',
+      // the proper extensions will be added
+      fileName: 'siwf',
+      formats: ['es', 'umd', 'cjs'],
+    },
+  },
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+    coverage: {
+      exclude: ['src/**/*.types.ts'],
+    },
+    globals: true,
+    environment: 'jsdom',
+    mockReset: true,
+  },
+});
