909dbeac908ac687cb822cbca8cc93d6399200f5
Wil Wade <wil.wade@projectliberty.io>
2024-10-23 13:42:21 -0400

    Fix the generator list of permissions and add additional data

--- tools/signed-request-generator/src/lib/schemas.ts
+++ tools/signed-request-generator/src/lib/schemas.ts
@@ -1,24 +1,232 @@
-// This has already applied the transformation from the v1.13.0 migration
-export const SCHEMA_NAME_TO_ID = new Map<string, number>([
-	['dsnp.broadcast@v1', 2],
-	['dsnp.broadcast@v2', 17],
-	['dsnp.dsnp-content-attribute-set@v1', 12],
-	['dsnp.ext-content-attribute-set@v1', 13],
-	['dsnp.private-connections@v1', 10],
-	['dsnp.private-follows@v1', 9],
-	['dsnp.profile-resources@v1', 15],
-	['dsnp.profile@v1', 6],
-	['dsnp.public-follows@v1', 8],
-	['dsnp.public-key-assertion-method@v1', 14],
-	['dsnp.public-key-key-agreement@v1', 7],
-	['dsnp.reaction@v1', 4],
-	['dsnp.reply@v1', 3],
-	['dsnp.reply@v2', 18],
-	['dsnp.tombstone@v1', 1],
-	['dsnp.tombstone@v2', 16],
-	['dsnp.update@v1', 5],
-	['dsnp.update@v2', 19],
-	['dsnp.user-attribute-set@v1', 11],
-	['dsnp.user-attribute-set@v2', 20],
-	['frequency.default-token-address@v1', 21]
-]);
+// If this is updated, you might also need to update Delegations.md via:
+// npx tsx tools/signed-request-generator/generate-available-delegations.ts
+
+export interface SchemaInfo {
+	description: string;
+	id: number;
+	name: string;
+	namespace: string;
+	version: number;
+	signatureRequired: boolean;
+	deprecated: boolean;
+	specUrl: string;
+}
+
+const schemaFullName = (info: SchemaInfo): string =>
+	`${info.namespace}.${info.name}@v${info.version}`;
+
+export const SCHEMA_INFOS: Map<string, SchemaInfo> = new Map(
+	[
+		{
+			description: 'Create new public content',
+			id: 2,
+			name: 'broadcast',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: false,
+			deprecated: true,
+			specUrl: 'https://spec.dsnp.org/DSNP/Types/Broadcast.html'
+		},
+		{
+			description: 'Create new public content',
+			id: 17,
+			name: 'broadcast',
+			namespace: 'dsnp',
+			version: 2,
+			signatureRequired: false,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/Types/Broadcast.html'
+		},
+		{
+			description: 'Create an authenticated attribute set for DSNP content',
+			id: 12,
+			name: 'dsnp-content-attribute',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: false,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/Types/DSNPContentAttributeSet.html'
+		},
+		{
+			description: 'Create an authenticated attribute set for content external to DSNP',
+			id: 13,
+			name: 'ext-content-attribute',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: false,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/Types/ExternalContentAttributeSet.html'
+		},
+		{
+			description: 'Update private friendship connections',
+			id: 10,
+			name: 'private-connections',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: false,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/UserData.html'
+		},
+		{
+			description: 'Update private follow list',
+			id: 9,
+			name: 'private-follows',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: false,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/UserData.html'
+		},
+		{
+			description: 'Update user profile information',
+			id: 15,
+			name: 'profile-resources',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: false,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/UserData.html'
+		},
+		{
+			description: 'Update profile information',
+			id: 6,
+			name: 'profile',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: false,
+			deprecated: true,
+			specUrl: 'https://spec.dsnp.org/DSNP/Types/Profile.html'
+		},
+		{
+			description: 'Update public follow list',
+			id: 8,
+			name: 'public-follows',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: false,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/UserData.html'
+		},
+		{
+			description: 'Used to verify generic cryptographic signatures',
+			id: 14,
+			name: 'public-key-assertion-method',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: true,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/UserData.html'
+		},
+		{
+			description: 'Publish new public key for the encrypted social graph',
+			id: 7,
+			name: 'public-key-key-agreement',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: true,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/UserData.html'
+		},
+		{
+			description: 'Public reaction to content',
+			id: 4,
+			name: 'reaction',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: false,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/Types/Reaction.html'
+		},
+		{
+			description: 'Public reply to a content',
+			id: 3,
+			name: 'reply',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: false,
+			deprecated: true,
+			specUrl: 'https://spec.dsnp.org/DSNP/Types/Reply.html'
+		},
+		{
+			description: 'Public reply to a content',
+			id: 18,
+			name: 'reply',
+			namespace: 'dsnp',
+			version: 2,
+			signatureRequired: false,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/Types/Reply.html'
+		},
+		{
+			description: 'Mark content for deletion',
+			id: 1,
+			name: 'tombstone',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: false,
+			deprecated: true,
+			specUrl: 'https://spec.dsnp.org/DSNP/Types/Tombstone.html'
+		},
+		{
+			description: 'Mark content for deletion',
+			id: 16,
+			name: 'tombstone',
+			namespace: 'dsnp',
+			version: 2,
+			signatureRequired: false,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/Types/Tombstone.html'
+		},
+		{
+			description: 'Update an existing post or reply',
+			id: 5,
+			name: 'update',
+			namespace: 'dsnp',
+			version: 1,
+			signatureRequired: false,
+			deprecated: true,
+			specUrl: 'https://spec.dsnp.org/DSNP/Types/Update.html'
+		},
+		{
+			description: 'Update an existing post or reply',
+			id: 19,
+			name: 'update',
+			namespace: 'dsnp',
+			version: 2,
+			signatureRequired: false,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/Types/Update.html'
+		},
+		// Never used
+		// {
+		// 	description: '',
+		// 	id: 11,
+		// 	name: 'user-attribute-set',
+		// 	namespace: 'dsnp',
+		// 	version: 1,
+		// 	signatureRequired: false,
+		// 	deprecated: true,
+		// specUrl: '',
+		// },
+		{
+			description: 'Create an authenticated attribute set for a DSNP User',
+			id: 20,
+			name: 'user-attribute-set',
+			namespace: 'dsnp',
+			version: 2,
+			signatureRequired: false,
+			deprecated: false,
+			specUrl: 'https://spec.dsnp.org/DSNP/Types/UserAttributeSet.html'
+		},
+		{
+			description: 'List one or more default token receiving addresses',
+			id: 21,
+			name: 'default-token-address',
+			namespace: 'frequency',
+			version: 1,
+			signatureRequired: true,
+			deprecated: false,
+			specUrl: 'https://github.com/frequency-chain/schemas'
+		}
+	].map((x) => [schemaFullName(x), x])
+);
