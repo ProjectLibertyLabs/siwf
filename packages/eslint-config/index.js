ba94a9fa3f479bc7f6e4cd98913b734d9b877882
shannonwells <shannonwells@users.noreply.github.com>
2025-10-17 15:07:01 -0700

    address copilot warning re: unbounded limit in regex for parseName

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,64 +1,95 @@
-module.exports = {
-  root: true,
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:svelte/recommended', 'prettier'],
-  ignorePatterns: [
-    '.DS_Store',
-    'node_modules',
-    'dist',
-    '/build',
-    '/.svelte-kit',
-    '/package',
-    '.env',
-    '.env.*',
-    '!.env.example',
-    'pnpm-lock.yaml',
-    'package-lock.json',
-    'yarn.lock',
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2020,
-    extraFileExtensions: ['.svelte'],
-  },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true,
-  },
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      // prevent variables with a _ prefix from being marked as unused
-      'warn',
-      {
-        varsIgnorePattern: '^_',
-        argsIgnorePattern: '^_',
-        destructuredArrayIgnorePattern: '^_',
-      },
-    ],
-    'no-extra-boolean-cast': 'off',
-    'sort-imports': [
-      'error',
-      {
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-      },
-    ],
-  },
-  overrides: [
-    {
-      files: ['*.svelte'],
-      parser: 'svelte-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-      },
+const {
+    defineConfig,
+    globalIgnores,
+} = require("@eslint/js/config");
+
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const globals = require("globals");
+const parser = require("svelte-eslint-parser");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([{
+    extends: compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:svelte/recommended",
+        "prettier",
+    ),
+
+    languageOptions: {
+        parser: tsParser,
+        sourceType: "module",
+        ecmaVersion: 2020,
+
+        parserOptions: {
+            extraFileExtensions: [".svelte"],
+        },
+
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
     },
-  ],
-  settings: {
-    'import/resolver': {
-      typescript: true,
-      node: true,
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
     },
-  },
-};
+
+    rules: {
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            varsIgnorePattern: "^_",
+            argsIgnorePattern: "^_",
+            destructuredArrayIgnorePattern: "^_",
+        }],
+
+        "no-extra-boolean-cast": "off",
+
+        "sort-imports": ["error", {
+            ignoreCase: true,
+            ignoreDeclarationSort: true,
+        }],
+    },
+
+    settings: {
+        "import/resolver": {
+            typescript: true,
+            node: true,
+        },
+    },
+}, globalIgnores([
+    "**/.DS_Store",
+    "**/node_modules",
+    "**/dist",
+    "build",
+    ".svelte-kit",
+    "package",
+    "**/.env",
+    "**/.env.*",
+    "!**/.env.example",
+    "**/pnpm-lock.yaml",
+    "**/package-lock.json",
+    "**/yarn.lock",
+]), {
+    files: ["**/*.svelte"],
+
+    languageOptions: {
+        parser: parser,
+
+        parserOptions: {
+            parser: "@typescript-eslint/parser",
+        },
+    },
+}]);
+
+module.exports = plugin;
