592809a3ac24f45f744da8b873b164c96760ae46
shannonwells <shannonwells@users.noreply.github.com>
2025-12-04 13:16:40 -0800

    fixing ci failures

--- libraries/js/vite.config.ts
+++ libraries/js/vite.config.ts
@@ -1,10 +1,13 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
     coverage: {
-      exclude: ['src/**/types.ts', 'src/index.ts', 'src/types/index.ts', 'src/mocks', './*.*s', 'scripts'],
+        enabled: true,
+        provider: 'v8',
+        exclude: ['src/**/types.ts', 'src/index.ts', 'src/types/index.ts', 'src/mocks', './*.*s', 'scripts'],
     },
     mockReset: true,
   },
